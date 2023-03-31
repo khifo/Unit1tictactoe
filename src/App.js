e6446ef321bfbb4c7c8f09cda7d57e0c167a51f6
khifo <khifo09@yahoo.com>
2023-03-31 13:19:24 -0600

    git Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Square from "./Square";
+function App() {
+  const propVariable = 'This is a prop'
+  return (
+    <div className="App">
+    <Square propVar={propVariable}/>
+);
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Square = () => {
+
+    return (
+        <div className="square">
+            {props.propsVar}
+        </div>
+    );
+};
+
+export default Square;
